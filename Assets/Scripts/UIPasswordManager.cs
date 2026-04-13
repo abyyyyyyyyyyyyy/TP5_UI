89f1fcbf11a89fa0f978335a4386bf95d8c498e2
Abril Avalos <49981547@est.ort.edu.ar>
2026-04-13 14:39:49 -0300

    SI
    HOLALALALALA

---
+++ Assets/Scripts/UIPasswordManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIPasswordManager : MonoBehaviour
+{
+    public GameObject txtResultadoContra;
+    public TextMeshProUGUI txtResultadoContraTexto;
+    public TMP_InputField input_Password;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultadoContra.SetActive(false);
+    }
+
+   public void ValidatePassword(){
+
+     txtResultadoContra.SetActive(true);
+     txtResultadoContraTexto.text = input_Password.text;
+
+    }
+
+   }
